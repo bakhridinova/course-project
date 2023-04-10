d34fcb826e050f613426c2af560256894a3c50c2
bakhridinova <bakhridinovaleyla@gmail.com>
2023-04-10 15:30:47 +0500

    initial commit

---
+++ src/main/java/com/example/bedclothesanddishwashers/BedclothesAndDishwashersApplication.java
@@ -0,0 +1,13 @@
+package com.example.bedclothesanddishwashers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedclothesAndDishwashersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BedclothesAndDishwashersApplication.class, args);
+    }
+
+}
