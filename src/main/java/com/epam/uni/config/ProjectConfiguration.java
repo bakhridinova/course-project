cf9c8f2bcf043546a630961d72b76f5dcfcae97c
bakhridinova <bakhridinovaleyla@gmail.com>
2023-04-25 12:38:32 +0500

    removed beans used for non - read operations

--- src/main/java/com/epam/uni/config/ProjectConfiguration.java
+++ src/main/java/com/epam/uni/config/ProjectConfiguration.java
@@ -2,10 +2,7 @@ package com.epam.uni.config;
 
 import com.epam.uni.entity.Dish;
 import com.epam.uni.entity.Linen;
-import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.bean.StatefulBeanToCsv;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,8 +11,6 @@ import org.springframework.context.annotation.Scope;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -60,59 +55,6 @@ public class ProjectConfiguration {
             throw new RuntimeException(e);
         }
         return new CsvToBeanBuilder<>(linenFileReader);
-    }
-
-    @Bean
-    public ColumnPositionMappingStrategy<Dish> dishMappingStrategy() {
-        ColumnPositionMappingStrategy<Dish> mappingStrategy =
-                new ColumnPositionMappingStrategy<>();
-        mappingStrategy.setType(Dish.class);
-        String[] columns = new String[]
-                {"id", "size", "price", "description",
-                        "left_in_stock", "category", "material"};
-        mappingStrategy.setColumnMapping(columns);
-        return mappingStrategy;
-    }
-
-    @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public StatefulBeanToCsv<Dish> dishBeanWriter(String dishFilePath,
-                                                  ColumnPositionMappingStrategy<Dish> dishMappingStrategy) {
-        FileWriter dishFileWriter;
-        try {
-            dishFileWriter = new FileWriter(dishFilePath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return new StatefulBeanToCsvBuilder<Dish>(dishFileWriter)
-                .withMappingStrategy(dishMappingStrategy).build();
-    }
-
-    @Bean
-    public ColumnPositionMappingStrategy<Linen> linenMappingStrategy() {
-        ColumnPositionMappingStrategy<Linen> mappingStrategy =
-                new ColumnPositionMappingStrategy<>();
-        mappingStrategy.setType(Linen.class);
-        String[] columns = new String[]
-                {"id", "price", "description",
-                        "left_in_stock", "category", "material"};
-        mappingStrategy.setColumnMapping(columns);
-        return mappingStrategy;
-    }
-
-    @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    public StatefulBeanToCsv<Linen> linenBeanWriter(String linenFilePath,
-                                                    ColumnPositionMappingStrategy<Linen> linenMappingStrategy) {
-        FileWriter linenFileWriter;
-        try {
-            linenFileWriter = new FileWriter(linenFilePath);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return new StatefulBeanToCsvBuilder<Linen>(linenFileWriter)
-                .withMappingStrategy(linenMappingStrategy).build();
     }
 
     @Bean
